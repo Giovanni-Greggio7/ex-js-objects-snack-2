89076d4e034dd59da3cb0c30744a2632a644ae86
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-04-30 11:53:33 +0200

    Add: snack 2

--- main.js
+++ main.js
@@ -9,39 +9,43 @@ console.log(secondBurger.name ); // 500
 
 // Senza lanciare il codice, riesci a prevedere cosa viene stampato in console? Si
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? Uno, é stato modificato tramite Reference
+
 // 🏆 Code Question 2
-// const hamburger = { 
-//     name: "Cheese Burger", 
-//     weight: 250,
-//     ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
-// };
+const hamburger = { 
+    name: "Cheese Burger", 
+    weight: 250,
+    ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+​
+const secondBurger = {...hamburger};
+secondBurger.ingredients[0] = "Salad";
 // ​
-// const secondBurger = {...hamburger};
-// secondBurger.ingredients[0] = "Salad";
-// ​
-// console.log(hamburger.ingredients[0]); // ?
-// console.log(secondBurger.ingredients[0]); // ?
+console.log(hamburger.ingredients[0]); // Salad
+console.log(secondBurger.ingredients[0]); // Salad
+
 // P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
-// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
-// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console? Si
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 3, ma l'oggetto annidato rimane come reference
+
+
 // 🏆 Code Question 3
-// const hamburger = { 
-//     name: "Cheese Burger", 
-//     weight: 250,
-//     maker: {
-//         name: "Anonymous Chef",
-//         restaurant: {
-//             name: "Hyur's Burgers",
-//             address: "Main Street, 123",
-//             isOpen: true,
-//         },
-//         age: 29
-//     }
-// };
-// ​
-// const secondBurger = structuredClone(hamburger);
-// const thirdBurger = structuredClone(hamburger);
-// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+const hamburger = { 
+    name: "Cheese Burger", 
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+​
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 
 // 🏆 Code Question 4
 // const chef = {
 //     name: "Chef Hyur",
